ca8e1aee760a51f89a561b9936f72e52549e4f23
vatan45 <vmro45@gmail.com>
2025-03-22 14:14:50 +0530

    some text xhanges

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,19 +1,19 @@
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "AMID AI Lab & Founders Fellowship",
-  description: "A dedicated space for building and collaboration, in the heart of San Francisco",
+  title: "CU TBI AI Lab & Founders Fellowship",
+  description: "A dedicated space for building and collaboration, in the heart of Chandigarh University",
   openGraph: {
-    title: "AMID AI Lab & Founders Fellowship",
-    description: "A dedicated space for building and collaboration, in the heart of San Francisco",
+    title: "CU TBI AI Lab & Founders Fellowship",
+    description: "A dedicated space for building and collaboration, in the heart of Chandigarh University",
     url: "https://www.amid.ai",
-    siteName: "AMID AI",
+    siteName: "CU TBI",
     locale: "en_US",
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: "AMID AI Lab & Founders Fellowship",
-    description: "A dedicated space for building and collaboration, in the heart of San Francisco",
+    title: "CU TBI AI Lab & Founders Fellowship",
+    description: "A dedicated space for building and collaboration, in the heart of Chandigarh University",
   },
 };
